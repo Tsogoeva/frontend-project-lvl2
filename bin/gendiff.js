d9c7d65ce8a08d3baf245897f6e581ccc1d2a5ed
Tsogoeva Tatiana <tsogoevats@gmail.com>
2022-03-22 17:12:26 +0300

    add action

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,5 +7,8 @@ program
   .version('0.0.1', '-V, --version', 'output the version number')
   .description('Compares two configuration files and shows a difference.')
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format <type>', 'output format');
+  .option('-f, --format <type>', 'output format')
+  .action((filepath1, filepath2, format) => {
+    console.log(gendiff(filepath1, filepath2, format.format));
+  });;
 program.parse();

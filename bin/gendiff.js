75156c0c3e954878dc8dbf6fd62ec70e95346881
Tsogoeva Tatiana <tsogoevats@gmail.com>
2022-03-22 18:13:57 +0300

    modify arguments

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,9 +6,10 @@ const program = new Command();
 program
   .version('0.0.1', '-V, --version', 'output the version number')
   .description('Compares two configuration files and shows a difference.')
-  .arguments('<filepath1> <filepath2>')
+  .argument('<filepath1>', 'path to file1')
+  .argument('<filepath2>', 'path to file2')
   .option('-f, --format <type>', 'output format')
-  .action((filepath1, filepath2, format) => {
-    console.log(gendiff(filepath1, filepath2, format.format));
+  .action((filepath1, filepath2) => {
+    console.log(gendiff(filepath1, filepath2));
   });;
 program.parse();

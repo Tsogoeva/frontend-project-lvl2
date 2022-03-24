89297c79f332e0336b830e529916b985c96556e7
Tsogoeva Tatiana <tsogoevats@gmail.com>
2022-03-24 13:59:21 +0300

    add function calculateDifferences

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
-import { readFileSync } from 'fs';
+import fs from 'fs';
+import path from 'path';
+import process from 'process';
+import _ from 'lodash';
+
+const readFile = (filepath) => (fs.readFileSync(path.resolve(process.cwd(), filepath), 'utf-8'));
 
 export default (filepath1, filepath2) => {
-  const data1 = readFileSync(filepath1, 'utf-8'); 
-  const data2 = readFileSync(filepath2, 'utf-8');
-  const dataParse1 = JSON.parse(data1);
-  const dataParse2 = JSON.parse(data2);
+  const content1 = readFile(filepath1);
+  const content2 = readFile(filepath2);
+  const dataParse1 = JSON.parse(content1);
+  const dataParse2 = JSON.parse(content2);
+
+  const calculateDifferences = (data1, data2) => {
+    const keys1 = Object.keys(data1);
+    const keys2 = Object.keys(data2);
+    const allKeys = _.union(keys1, keys2).sort();
+
+    const result = allKeys.map((key) => {
+      if (!Object.hasOwn(data1, key)) {
+        return `  + ${key}: ${data2[key]}`;
+      } if (!Object.hasOwn(data2, key)) {
+        return `  - ${key}: ${data1[key]}`;
+      } if (data1[key] !== data2[key]) {
+        return `  - ${key}: ${data1[key]}\n  + ${key}: ${data2[key]}`;
+      }
+      return `    ${key}: ${data1[key]}`;
+    });
+    return `{\n${result.join('\n')}\n}`;
+  };
+  return calculateDifferences(dataParse1, dataParse2);
 };

b1e51c32cc1fa45778e7ec90ddd1f50bc991b085
Tsogoeva Tatiana <tsogoevats@gmail.com>
2022-03-29 12:40:07 +0300

    add test for yml

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,6 +2,10 @@ import { test, expect } from '@jest/globals';
 import gendiff from '../src/index.js';
 import readFile from '../src/path.js';
 
-test('index', () => {
+test('check gendiff for json-file', () => {
   expect(gendiff('file1.json', 'file2.json')).toBe(readFile('result-json-file.txt'));
+});
+
+test('check gendiff for yaml-file', () => {
+  expect(gendiff('file1.yaml', 'file2.yaml')).toBe(readFile('result-json-file.txt'));
 });

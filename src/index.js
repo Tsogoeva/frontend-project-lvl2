b6bbc51616f796fd6feba95fa86f758e2cb19de0
Tsogoeva Tatiana <tsogoevats@gmail.com>
2022-04-04 19:44:32 +0300

    add func formatter

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import parse from './parsers.js';
 import buildTree from './tree.js';
+import makeStylish from './formatter-stylish.js';
 
 export default (filepath1, filepath2) => {
   const dataParse1 = parse(filepath1);
   const dataParse2 = parse(filepath2);
   const buildedTree = buildTree(dataParse1, dataParse2);
-  return buildedTree;
+  const stylishTree = makeStylish(buildedTree);
+  return stylishTree;
 };

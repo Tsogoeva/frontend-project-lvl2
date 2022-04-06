61d48d6f56858add7f3ab0e059b1659df6c6164b
Tsogoeva Tatiana <tsogoevats@gmail.com>
2022-04-06 19:14:18 +0300

    add default value

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,9 +8,9 @@ program
   .version('0.0.1', '-V, --version', 'output the version number')
   .description('Compares two configuration files and shows a difference.')
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format <type>', 'output format')
-  .action((filepath1, filepath2) => {
-    console.log(gendiff(filepath1, filepath2));
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .action((filepath1, filepath2, format) => {
+    console.log(gendiff(filepath1, filepath2, format));
   });
 program.parse();
 
--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ import parse from './parsers.js';
 import buildTree from './tree.js';
 import makeStylish from './formatter-stylish.js';
 
-export default (filepath1, filepath2) => {
+export default (filepath1, filepath2, format = 'stylish') => {
   const dataParse1 = parse(filepath1);
   const dataParse2 = parse(filepath2);
   const buildedTree = buildTree(dataParse1, dataParse2);
-  const stylishTree = makeStylish(buildedTree);
-  return stylishTree;
+  return makeStylish(buildedTree);
 };

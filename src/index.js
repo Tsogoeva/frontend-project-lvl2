d7b77ee1745c860a9fd5b729441f6624a5948757
Tsogoeva Tatiana <tsogoevats@gmail.com>
2022-04-08 14:04:56 +0300

    debugging

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ export default (filepath1, filepath2, formatName = 'stylish') => {
   const dataParse1 = parse(filepath1);
   const dataParse2 = parse(filepath2);
   const buildedTree = buildTree(dataParse1, dataParse2);
+  // console.log(JSON.stringify(buildedTree, null, '  '));
   // selectFormat(buildedTree, format);
   return getFormatted(buildedTree, formatName);
 };

7626ab1750dcfc36e26ccbb8d294915758b3188c
Tsogoeva Tatiana <tsogoevats@gmail.com>
2022-04-10 18:41:35 +0300

    change method sort to sortBy

--- src/tree.js
+++ src/tree.js
@@ -3,9 +3,10 @@ import _ from 'lodash';
 const buildTree = (data1, data2) => {
   const keys1 = Object.keys(data1);
   const keys2 = Object.keys(data2);
-  const allKeys = _.union(keys1, keys2).sort();
+  const allKeys = _.union(keys1, keys2);
+  const sortedKeys = _.sortBy(allKeys);
 
-  const result = allKeys.map((key) => {
+  const result = sortedKeys.map((key) => {
     const value1 = data1[key];
     const value2 = data2[key];
 
